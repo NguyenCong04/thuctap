74a7d964ed082c02db7a7055c5fa36c36926baf4
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-09-12 12:21:11 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,7 +40,7 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
-->withRouting(
+$app->withRouting(
     web: __DIR__.'/../routes/web.php',
     commands: __DIR__.'/../routes/console.php',
     health: '/status', 
